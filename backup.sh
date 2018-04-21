a3dd50fbef717daf0b97ca67672e7d765b9443f5
Martin Nestorov <mdinestorov@gmail.com>
2018-04-21 16:43:45 +0300

    Fixed backup command

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
-!#/bin/bash
+#!bin/bash
 
-lxc snapshot xwiki1 xwiki1-snap$(date +%Y%m%d)
-sudo zfs send lxd/main/containers/xwiki1@xwiki1-snap$(date +%Y%m%d)
+lxc snapshot xwiki1 xwiki1-snap$(date +%Y%m%d) > /dev/null 2>&1
+sudo zfs send lxd/main/containers/xwiki1@xwiki1-snap$(date +%Y%m%d) > /dev/null 2>&1 
 
-lxc snapshot xwiki2 xwiki2-snap$(date +%Y%m%d)
-sudo zfs send lxd/main/containers/xwiki2@xwiki2-snap$(date +%Y%m%d)
+lxc snapshot xwiki2 xwiki2-snap$(date +%Y%m%d) > /dev/null 2>&1
+sudo zfs send lxd/main/containers/xwiki2@xwiki2-snap$(date +%Y%m%d) > /dev/null 2>&1
 
-lxc snapshot monitor monitor-snap$(date +%Y%m%d)
-sudo zfs send lxd/main/containers/monitor@monitor-snap$(date +%Y%m%d)
+lxc snapshot monitor monitor-snap$(date +%Y%m%d) > /dev/null 2>&1
+sudo zfs send lxd/main/containers/monitor@monitor-snap$(date +%Y%m%d) > /dev/null 2>&1
